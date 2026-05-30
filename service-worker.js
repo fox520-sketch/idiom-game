90e7faf3f970cc9be0d8b567bb99303a9918a8b2
fox520-sketch <fox520486@gmail.com>
2026-05-30 10:45:54 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,13 @@
-const CACHE_NAME = 'idiom-game-v3-3-cache';
-const ASSETS = ['./', './index.html', './idioms.js', './README.md', './manifest.json'];
+const CACHE_NAME = 'idiom-game-v3-4-cache';
+const ASSETS = ['./', './index.html', './idioms.js', './README.md', './manifest.json', './service-worker.js'];
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 self.addEventListener('activate', event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
 });
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
