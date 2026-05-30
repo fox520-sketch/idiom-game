c857f5b9b1f36187f18cd5502ed11badbbf16558
fox520-sketch <fox520486@gmail.com>
2026-05-30 15:18:54 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase 設定檔
+// 使用多人連線功能前，請到 Firebase Console 建立 Web App，
+// 把 firebaseConfig 內容貼到下面。Firebase config 可放在前端，真正的安全性請靠 Firestore Rules 控制。
+window.firebaseConfig = {
+  apiKey: "請填入你的 apiKey",
+  authDomain: "請填入你的 authDomain",
+  projectId: "請填入你的 projectId",
+  storageBucket: "請填入你的 storageBucket",
+  messagingSenderId: "請填入你的 messagingSenderId",
+  appId: "請填入你的 appId"
+};
