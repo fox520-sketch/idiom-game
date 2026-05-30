72ea27e4376ffa26598ae33f6b89f9461bf97806
fox520-sketch <fox520486@gmail.com>
2026-05-30 16:19:37 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'idiom-game-v3-7-cache';
+const CACHE_NAME = 'idiom-game-v3-7-3-cache';
 const ASSETS = ['./', './index.html', './idioms.js', './README.md', './manifest.json', './service-worker.js', './firebase-config.js'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
